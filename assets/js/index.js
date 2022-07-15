6f455c1aaa6926c87279a549122d01ac5c163357
laurasierra17 <laura.sierra17@gmail.com>
2022-07-15 14:27:34 -0400

    promp user for their personal information

---
+++ assets/js/index.js
@@ -0,0 +1,33 @@
+// Initialize modules
+const fs = require('file-system');
+const inquirer = require('inquirer');
+
+// Prompt user for their name, location, bio, and URLs to their LinkedIn and GitHub
+inquirer
+    .prompt([
+        {
+            message: "What is your first name?",
+            name: "firstName"
+        },
+        {
+            message: "Where are you located?",
+            name: "location"
+        },
+        {
+            message: "How would you introduce yourself in one sentence?",
+            name: "bio"
+        },
+        {
+            message: "What is your LinkedIn username?",
+            name: "linkedin"
+        },
+        {
+            message: "What is your GitHub username?",
+            name: "github"
+        }
+    ])
+    .then(({ firstName, location, bio, linkedin, github }) => {
+        // Save user's response in a string
+        
+
+    })
